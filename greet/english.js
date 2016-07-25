65ccf943edfa08c398353577649552e71c7e91df
markpj1 <markpj@hotmail.com>
2016-07-24 20:34:29 -0700

    Require function/object

---
+++ greet/english.js
@@ -0,0 +1,5 @@
+var greet = function () {
+    console.log('English hello world');
+};
+
+module.exports = greet;
---
+++ greet/greet.js
@@ -0,0 +1,5 @@
+var greet = function () {
+  console.log('Hello World inside greet function');
+};
+
+module.exports = greet;
